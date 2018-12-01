0666006cf633e62cfb0d10b3875032f1052b0825
Marcelino Verissimo <mvsjk9@hotmail.com>
2018-12-01 16:23:35 -0300

    Final

--- Teste em Java/Grammar3/src/compiler/Semantico.java
+++ Teste em Java/Grammar3/src/compiler/Semantico.java
@@ -1,30 +1,40 @@
 package compiler;
 
 import java.util.Stack;
+import java.util.TreeMap;
 
 public class Semantico implements Constants {
 
 	Stack<Integer> stack = new Stack();
-
-	public int getResult() {
-		return ((Integer) stack.peek()).intValue();
-	}
+	TreeMap<String, Integer> variavel = new TreeMap<>();
+	boolean existe = false, temValor = false;
+	String var = "";
+	
 
 	public void executeAction(int action, Token token) throws SemanticError {
 
-		Integer a, b, c = 0, result = 0;
-
+		Integer a, b;
+		
 		switch (action) {
 
-		case 1:
-			
+		case 1:			
 			break;
 		case 2:
-
 			break;
 		case 3:
 			break;
 		case 4:
+			if(existe) 
+			{
+				variavel.put(var, stack.pop());
+				existe = false;
+			}
+			if(temValor) 
+			{
+				variavel.remove(var);
+				variavel.put(var, stack.pop());
+				temValor = false;
+			}
 			break;
 		case 5:
 			System.out.println(stack.pop());
@@ -56,16 +66,30 @@ public class Semantico implements Constants {
 		case 12:
 			break;
 		case 13:
+			if(variavel.containsKey(token.getLexeme()))
+			{
+				int val = variavel.get(token.getLexeme());
+				stack.push(val);
+			}else {
+				throw new SemanticError("Variável não foi inicializada!");
+			}
 			break;
 		case 14:
 			String tmp = token.getLexeme();
 			stack.push(Integer.valueOf(tmp));
 			break;
 		case 15:
+			var = token.getLexeme();
+			if(variavel.containsKey(var))
+			{
+				temValor = true;
+			}else{
+				existe = true;
+			}
 			break;
 
 		}
 
-	System.out.println("Ação #" + action + ", Token: " + token);
+	//System.out.println("Ação #" + action + ", Token: " + token);
 	}
 }
